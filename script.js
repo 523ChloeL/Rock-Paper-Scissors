b7e32c7c8c32102abc2fd5df99d16d05801ac958
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-04-01 10:32:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//complete game function
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+// function to grab user inputadn run function to evaluate choices
+  const playGame = () => {
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorBtn = document.querySelector('.scissor');
+  }
+}
