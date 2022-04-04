d6df5cb3e6231b94a4367ce45b2a69484334681d
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-04-04 11:56:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,4 +8,29 @@ const game = () => {
     const paperBtn = document.querySelector('.paper');
     const scissorBtn = document.querySelector('.scissor');
   }
+  let pScoreboard = document.querySelector('.p-count');
+  let cScoreboard = document.querySelector('.c-count');
+  let roundResult = document.querySelector('.result');
+  //initalizing global variables to store user and computer selection
+  let compChoice;
+  let playerChoice;
+
+  //function to randomly generate a choice for the computer, use this function
+  function computerChoice() {
+    const computerOptions = ["rock","paper", "scissor"];
+    const randomNum = Math.floor(Math.random()*3);
+    compChoice = computerOptions[randomNum];
+  }
+  function playerChoice() {
+
+  }
+  function throwRock() {
+    
+  }
+  function throwPaper() {
+
+  }
+  function throwScissor() {
+
+  }
 }
