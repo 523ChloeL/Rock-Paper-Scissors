6ca84f2787563b4b443dddc046464c203a4fd9f1
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-04-09 00:09:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,35 +15,64 @@
   let compChoice;
   let playerChoice;
 
+  function pWin() {
+    playerScore++;
+    pScoreboard.innerHTML = playerScore;
+    roundResult.innerHTML = "Player Wins!"
+  }
+  function cWin() {
+    computerScore++;
+    pScoreboard.innerHTML = computerScore;
+    roundResult.innerHTML = "Computer Wins!"
+  }
   //function to randomly generate a choice for the computer, use this function
   function computerChoice() {
     const computerOptions = ["rock","paper", "scissor"];
     const randomNum = Math.floor(Math.random()*3);
     compChoice = computerOptions[randomNum];
+    document.getElementById('computer').style.backgroundImage = 'url(img/' computerChoice.value '.jpg)';
   }
-  function playerChoice() {
-    const playerOptions = ['rock','paper','scissor'];
-    if (rockBtn) {
-      document.getElementById('player').style.backgroundImage = 'url(img/rock.jpg)';
-      playerScore ++;
-      playerChoice = 'rock';
-    }
+  function paper() {
+    playerChoice = "paper";
+    document.getElementById('player').style.backgroundImage = 'url(img/paper.jpg)';
+    playGame();
+
+  }
+  function scissor() {
+    playerChoice = "scissor";
+    document.getElementById('player').style.backgroundImage = 'url(img/scissor.jpg)';
+    playGame();
+  }
+  function rock() {
+    playerChoice = "rock";
+    document.getElementById('player').style.backgroundImage = 'url(img/rock.jpg)';
+    playGame();
+  }
+  function playGame() {
+    computerChoice();
+    game (playerChoice, compChoice);
+  }
+  function resetScore () {
+    computerScore = 0;
+    playerScore = 0;
+    cScoreboard.innerHTML = computerScore;
+    pScoreboard.innerHTML = playerScore;
   }
 
 function game() {
   if (playerChoice === computerChoice)
   document.getElementById("result").textContent="It's a Tie!";
 }
-else if (playerChoice == rockBtn && computerChoice == scissorBtn){
-  document.getElementById("result").textContent="Player Wins!";
+else if (playerChoice === "rock" && computerChoice === "scissor"){
+  pWin();
 }
-else if (playerChoice == paperBtn && computerChoice == rockBtn){
-  document.getElementById("result").textContent="Player Wins!";
+else if (playerChoice === "paper" && computerChoice === "rock"){
+  pWin();
 }
-else if (playerChoice == scissorBtn && computerChoice == paperBtn){
-  document.getElementById("result").textContent="Player Wins!";
+else if (playerChoice === "scissor" && computerChoice === "paper"){
+  pWin();
 } else {
-  document.getElementById("result").textContent="Computer Wins!";
+  cWin();
 }
 }
 playGame();
